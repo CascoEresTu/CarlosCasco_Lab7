3ebcb317a20f946163bc751846d723cce9836f93
Carlos Guillermo Casco Valle <k_k_ryuten@live.com>
2016-12-02 17:55:05 -0600

    lectura de archivos de texto terminda

--- main.cpp
+++ main.cpp
@@ -6,11 +6,16 @@
 #include <iostream>
 #include <fstream>
 #include <istream>
-
+#include <string>
 #include <vector>
 #include <typeinfo>
+#include <cmath>
+#include<stdlib.h>
 
 using namespace std;
+
+int stringAint(string &numero);
+
 
 void guardarDatos(vector<User*> usuarios ){
 
@@ -31,26 +36,26 @@ void guardarDatos(vector<User*> usuarios ){
 
             if (dynamic_cast<Admin*> (usuarios.at(i))!=NULL){
                 Admin* temp = dynamic_cast<Admin*> (usuarios.at(i));
-                admin<< temp->getName()<<"\n"<< temp->getEmail() <<"\n"<<temp->getPassword() << "\n"<< temp->getDate() << endl;
+                admin<< temp->getName()<<";"<< temp->getEmail() <<";"<<temp->getPassword() << ";"<< temp->getDate() << endl;
 
             }
 
             if (dynamic_cast<Manager*> (usuarios.at(i))!=NULL){
                 Manager* temp = dynamic_cast<Manager*> (usuarios.at(i));
-                manager<< temp->getName()<<"\n"<< temp->getEmail() <<"\n"<<temp->getPassword() << "\n"<< temp->getSalary() << endl;
+                manager<< temp->getName()<<";"<< temp->getEmail() <<";"<<temp->getPassword() << ";"<< temp->getSalary() << endl;
 
 
             }
 
             if (dynamic_cast<Intern*> (usuarios.at(i))!=NULL){
                 Intern* temp = dynamic_cast<Intern*> (usuarios.at(i));
-                intern<< temp->getName()<<"\n"<< temp->getEmail() <<"\n"<<temp->getPassword() << "\n"<< temp->getDaysWorked() << endl;
+                intern<< temp->getName()<<";"<< temp->getEmail() <<";"<<temp->getPassword() << ";"<< temp->getDaysWorked() << endl;
 
             }
 
             if (dynamic_cast<Supervisor*> (usuarios.at(i))!=NULL){
                 Supervisor* temp = dynamic_cast<Supervisor*> (usuarios.at(i));
-                supervisor<< temp->getName()<<"\n"<< temp->getEmail() <<"\n"<<temp->getPassword() << "\n"<< temp->getEntradas() << endl;
+                supervisor<< temp->getName()<<";"<< temp->getEmail() <<";"<<temp->getPassword() << ";"<< temp->getEntradas() << endl;
             }
         }
 
@@ -69,13 +74,128 @@ void guardarDatos(vector<User*> usuarios ){
 
 void cargarDatos(vector<User*> usuarios){
 
-    ifstream admin;
-    ifstream manager;
-    ifstream intern;
-    ifstream supervisor;
+string token;
+//ifstream file("file.txt");
 
 
-    std::cout << "bump" << std::endl;
+    ifstream admin("Admins.txt");
+    ifstream manager("Managers.txt");
+    ifstream intern("Interns.txt");
+    ifstream supervisor("Supervisors.txt");
+
+    ifstream file;
+
+//    file.open("Admins.txt");
+    string line;
+
+    while(!admin.eof()){
+
+    getline(admin,line);
+    //std::cout << line << std::endl;
+        std::string delimiter = ";";
+        size_t pos = 0;
+        std::string token;
+        //partir en tokens
+        string nombre;
+        string correo;
+        string contra;
+        string fecha;
+
+        if(admin.fail()){
+            std::cout << "falla" << std::endl;
+        }else{
+            getline(admin,nombre,';');
+            getline(admin,correo,';');
+            getline(admin,contra,';');
+            getline(admin,fecha,';');
+        }
+        usuarios.push_back(new Admin(nombre,correo,contra,fecha));
+}
+    admin.close();
+
+
+
+    while(!intern.eof()){
+
+    getline(intern,line);
+    //std::cout << line << std::endl;
+        std::string delimiter = ";";
+        size_t pos = 0;
+        std::string token;
+        //partir en tokens
+        string nombre;
+        string correo;
+        string contra;
+        string fecha;
+
+        if(intern.fail()){
+            std::cout << "falla" << std::endl;
+        }else{
+            getline(intern,nombre,';');
+            getline(intern,correo,';');
+            getline(intern,contra,';');
+            getline(intern,fecha,';');
+        }
+        double numero  = atof(fecha.c_str());
+        usuarios.push_back(new Intern(nombre,correo,contra,numero));
+    }
+    intern.close();
+
+
+    while(!manager.eof()){
+
+    getline(manager,line);
+    //std::cout << line << std::endl;
+        std::string delimiter = ";";
+        size_t pos = 0;
+        std::string token;
+        //partir en tokens
+        string nombre;
+        string correo;
+        string contra;
+        string fecha;
+
+        if(manager.fail()){
+            std::cout << "falla" << std::endl;
+        }else{
+            getline(manager,nombre,';');
+            getline(manager,correo,';');
+            getline(manager,contra,';');
+            getline(manager,fecha,';');
+        }
+        int numero  = atoi(fecha.c_str());
+        usuarios.push_back(new Manager(nombre,correo,contra,numero));
+    }
+    manager.close();
+
+
+    while(!supervisor.eof()){
+
+    getline(supervisor,line);
+    //std::cout << line << std::endl;
+        std::string delimiter = ";";
+        size_t pos = 0;
+        std::string token;
+        //partir en tokens
+        string nombre;
+        string correo;
+        string contra;
+        string fecha;
+
+        if(supervisor.fail()){
+            std::cout << "falla" << std::endl;
+        }else{
+            getline(supervisor,nombre,';');
+            getline(supervisor,correo,';');
+            getline(supervisor,contra,';');
+            getline(supervisor,fecha,';');
+        }
+        int numero  = atoi(fecha.c_str());
+        usuarios.push_back(new Supervisor(nombre,correo,contra,numero));
+    }
+    supervisor.close();
+
+
 
 }
 
@@ -85,11 +205,32 @@ int main(int argc, char const *argv[]) {
     Admin* Juan = new Admin("Juan","cgc@asd.com","kakaroto", "Martes");
     Admin* pJuan = new Admin("Juan","cgc@asd.com","kakaroto", "Martes");
 
+
     usuarios.push_back(Juan);
     usuarios.push_back(pJuan);
 
     std::cout << "hola" << std::endl;
     guardarDatos(usuarios);
+    cargarDatos(usuarios);
 
     return 0;
+}
+
+
+int stringAint(string &numero){
+    if (numero.length()==1){
+        return numero[0] - 48;
+    }else {
+        int asd = numero.length()-1 ;
+        int suma = 0 ;
+
+        for (size_t i = 0; i < numero.length(); i++) {
+            if(numero[i] != '\0'){
+                suma+=  (numero[i] - 48) * pow(10,asd);
+                asd--;
+            }
+        }
+        std::cout << suma << std::endl;
+        return suma;
+    }
 }
